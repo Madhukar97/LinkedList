f4dd5f92e2feadfcc69b6b84337ba8c378b65728
unknown <madhukar.tatiraju@gmail.com>
2021-11-02 00:08:50 +0530

    UC1 added

---
+++ src/main/java/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList obj = new LinkedList();
+        obj.push(70);
+        obj.push(30);
+        obj.push(56);
+        System.out.println("The current LinkedList is: ");
+        obj.showLinkedList();
+    }
+}
