5627d7051b3aab4f5a0b8eab900ea9fb74744383
unknown <madhukar.tatiraju@gmail.com>
2021-11-07 13:22:28 +0530

    Stack UC2 Peak and Pop methods added

--- src/main/java/com/bridgelabz/stack/Stack.java
+++ src/main/java/com/bridgelabz/stack/Stack.java
@@ -22,13 +22,31 @@ public class Stack<T> {
         System.out.println(linkedList.head.data);
     }
 
+    /**
+     * Pop method is internally calling the LinkedList pop method
+     */
+    public T pop() {
+        return linkedList.pop();
+    }
+
+    /**
+     * Size method is internally calling the LinkedList size method
+     */
+    public void size() {
+        linkedList.size();
+    }
+
     public static void main(String[] args) {
         Stack<Integer> obj = new Stack<>();
 
         obj.push(70);
         obj.push(30);
         obj.push(56);
-        obj.peak();
+        while (obj.linkedList.head != null) {
+            System.out.print("Peak element= ");
+            obj.peak();
+            System.out.println("Popped element = " + obj.pop());
+        }
+        obj.size();
     }
-
 }
